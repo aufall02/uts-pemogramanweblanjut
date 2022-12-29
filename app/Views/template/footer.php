46ad4b4356d19d192c18c444cdda75229bf46849
aufall02 <aufaluzumaki@gmail.com>
2022-12-30 04:07:04 +0700

    update

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,6 +1,4 @@
 
-
-
 <footer class="p-4 bg-white sm:p-6 dark:bg-gray-900">
     <div class="sm:flex sm:items-center sm:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Aufal marom™</a>. Uts web lanjut.
